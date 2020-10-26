af2d93c080ccf318907bf1877d152344e87c76b9
WolfMAN <aria.azadi.pour@protonmail.com>
2020-10-26 15:50:05 +0330

    create relations for deleting

--- src/prisma.js
+++ src/prisma.js
@@ -1,6 +1,6 @@
 import {Prisma} from "prisma-binding";
 
 const prisma = new Prisma({
-    typeDefs: 'src/generated/prisma.graphql',
+    typeDefs: 'src/generated/schema.graphql',
     endpoint: 'http://127.0.0.1:4466'
 });
