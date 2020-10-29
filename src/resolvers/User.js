dc393d152dad0fd021a7a3a67ef71235a286e21d
WolfMAN <aria.azadi.pour@protonmail.com>
2020-10-30 00:56:18 +0330

    hide email for users

--- src/resolvers/User.js
+++ src/resolvers/User.js
@@ -1,2 +1,10 @@
+import protect from "../utils/auth/protect";
+
 export default {
+    email(parent, args, {prisma: {query}, request}, info) {
+        const user = protect(request, query, false);
+
+        if (user && parent.id === user.id) return parent.email;
+        return null;
+    }
 };
